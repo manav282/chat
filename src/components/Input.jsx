1ce40435531295fee1b57918d8e3eb457e049c68
Manav Majithiya <91357474+manav282@users.noreply.github.com>
2023-03-14 21:16:37 +0530

    Add files via upload

---
+++ src/components/Input.jsx
@@ -0,0 +1,138 @@
+import React, { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import Attach from "../img/attach.png";
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
+const Input = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+  const [isRecording, setisRecording] = useState(false);
+
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  const microphone = new SpeechRecognition();
+
+  microphone.continuous = true;
+  microphone.interimResults = true;
+  microphone.lang = "en-US";
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (img) {
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    setText("");
+    setImg(null);
+  };
+
+  const startRecordController = () => {
+    if (isRecording) {
+      microphone.start();
+    } else {
+      microphone.stop();
+      microphone.onend = () => {
+        console.log("Stopped microphone on Click");
+      };
+    }
+
+    microphone.onresult = (event) => {
+      const recordingResult = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      console.log(recordingResult);
+      setText(recordingResult);
+      microphone.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  };
+
+  useEffect(() => {
+    startRecordController();
+  }, [isRecording]);
+
+  return (
+    <div className="input">
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+
+      <div className="send">
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
+        <label htmlFor="file">
+          <img src={Attach} alt="" />
+        </label>
+        {isRecording ? <span>Recording... </span> : <span>Stopped </span>}
+        <button onClick={() => setisRecording((prevState) => !prevState)}>
+          Start/Stop
+        </button>
+        <button onClick={handleSend}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
